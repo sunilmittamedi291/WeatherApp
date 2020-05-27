0e7031b2e5cd1c5187cc16c852a09a24264e61b1
sunilmittamedi291 <sunil.mittamedi2@gmail.com>
2020-05-27 10:57:09 +0530

    Initial Commit

---
+++ WeatherSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherSampleApp
+//
+//  Created by sunilreddy on 27/05/20.
+//  Copyright © 2020 Sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
